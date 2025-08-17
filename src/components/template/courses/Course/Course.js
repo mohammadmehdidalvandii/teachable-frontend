f55920136656de8c01d7aa4ce432b41e75781b62
mohammadmehiddalvadnii <mhmdmehdidalvandii@gmail.com>
2025-08-17 16:39:17 +0330

    created pages courses is success

--- src/components/template/courses/Course/Course.js
+++ src/components/template/courses/Course/Course.js
@@ -1,13 +1,66 @@
-import React from 'react';
+"use client"
+import React, { useState } from 'react';
 import style from './Course.module.css';
+import { FaSearch } from 'react-icons/fa';
+import CourseCart from '@/components/modules/CourseCart/CourseCart';
 
 function Course() {
+    const [free , setFree] = useState(false);
+
+
   return (
     <section className={style.course}>
         <div className="container">
             <div className={style.course_grid}>
-                <div className={style.course_grid_right}>this right</div>
-                <div className={style.course_grid_left}>this left</div>
+                <div className={style.course_grid_right}>
+                    <div className={style.course_right}>
+                                            <div className={style.courseBox}>
+                        <div className={style.course_search}>
+                            <input type="text" placeholder='جستجو بین دوره ها ...'/>
+                            <button><FaSearch/></button>
+                        </div>
+                    </div>
+                    <div className={style.courseBox}>
+                        <div className={style.course_toggle}>
+                            <span className={style.course_title}>فقط دوره های رایگان</span>
+                            <label  className={style.switch}>
+                                <input type="checkbox" className={style.switch_input} checked={free}  onChange={()=>setFree(!free)}/>
+                                <span className={style.switch_slider}></span>
+                            </label>
+                        </div>
+                    </div>
+                    <div className={style.courseBox}>
+                        <div className={style.course_toggle}>
+                            <span className={style.course_title}>در حال پیش فروش</span>
+                            <label  className={style.switch}>
+                                <input type="checkbox"  />
+                                <span className={style.switch_slider}></span>
+                            </label>
+                        </div>
+                    </div>
+                    <div className={style.courseBox}>
+                        <div className={style.course_toggle}>
+                            <span className={style.course_title}>دوره ها خریداری شده</span>
+                            <label  className={style.switch}>
+                                <input type="checkbox"  />
+                                <span className={style.switch_slider}></span>
+                            </label>
+                        </div>
+                    </div>
+                    </div>
+                </div>
+                <div className={style.course_grid_left}>
+                    <div className={style.courses_items}>
+                        <CourseCart/>
+                        <CourseCart/>
+                        <CourseCart/>
+                        <CourseCart/>
+                        <CourseCart/>
+                        <CourseCart/>
+                    </div>
+
+                    <button className={`${style.course_btn} ${'btn primary'}`}>مشاهده بیشتر</button>
+                </div>
             </div>
         </div>
     </section>
