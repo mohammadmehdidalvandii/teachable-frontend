5ea497dec2b966c6e495e1e52d6cc3ce09f7a5e8
mohammadmehiddalvadnii <mhmdmehdidalvandii@gmail.com>
2025-08-18 14:06:42 +0330

    created page courses [id] is success

---
+++ src/app/Courses/[id]/page.js
@@ -0,0 +1,14 @@
+import Footer from '@/components/modules/Footer/Footer'
+import Navbar from '@/components/modules/Navbar/Navbar'
+import React from 'react'
+
+function page() {
+  return (
+    <>
+    <Navbar/>
+    <Footer/>
+    </>
+  )
+}
+
+export default page
