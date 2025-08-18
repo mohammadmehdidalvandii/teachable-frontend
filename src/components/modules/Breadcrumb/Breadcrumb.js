41f9ca8fdc42368463b77603f9613c9dd25fe734
mohammadmehiddalvadnii <mhmdmehdidalvandii@gmail.com>
2025-08-18 14:20:57 +0330

    fixed breadcrumbs

--- src/components/modules/Breadcrumb/Breadcrumb.js
+++ src/components/modules/Breadcrumb/Breadcrumb.js
@@ -3,14 +3,14 @@ import style from './Breadcrumb.module.css';
 import Link from 'next/link';
 import { FaHome } from 'react-icons/fa';
 
-function Breadcrumb() {
+function Breadcrumb({title}) {
   return (
     <div className={style.breadcrumb}>
         <div className="container">
             <div className={style.breadcrumb_wrapper}>
                 <Link href='/' className={style.breadcrumb_linkBack}><FaHome/></Link>
                 
-                <p className={style.breadcrumb_title}>آموزش جامع توسعه وردپرس</p>
+                <p className={style.breadcrumb_title}>{title}</p>
             </div>
         </div>
     </div>
